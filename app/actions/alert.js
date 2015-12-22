49d53a9532e03a30cda2783b9322437baa746b43
Izaak Sofer <izaakrogan@googlemail.com>
2015-12-22 17:50:37 +0000

    actions alert added to actions

---
+++ app/actions/alert.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import * as types from '../action_types.js';
+
+export const showAlert = () => {
+  return { type: types.ALERT, alert: 'test' };
+}
+
+export const clearAlert = () => {
+  return { type: types.CLEAR_ALERT };
+}
