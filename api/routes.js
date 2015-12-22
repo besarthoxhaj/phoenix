162fa9f0831b379de05ed0d00f141b8506a34354
Nikhila Ravi <nikkiravi92@gmail.com>
2015-12-22 17:32:32 +0000

    Adds login route

--- api/routes.js
+++ api/routes.js
@@ -1,10 +1,17 @@
 module.exports = (o) => {
-  const push    = require('./handlers/push.js')(o.Apn);
+  const push  = require('./handlers/push.js')(o.Apn);
+  const login = require('./handlers/login.js');
+
   return [
     {
       method: 'POST',
       path: '/push',
       handler: push,
+    },
+    {
+      method: 'POST',
+      path: '/login',
+      handler: login,
     },
   ];
 };
