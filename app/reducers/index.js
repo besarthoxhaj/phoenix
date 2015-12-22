0e5f989b2ef76304a24f4de3e1ab23e863b93cfb
Izaak Sofer <izaakrogan@googlemail.com>
2015-12-22 17:57:05 +0000

    add alert to index reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-import { combineReducers } from 'redux';
-import { reducer as store } from './store.js';
+import { combineReducers }       from 'redux';
+import { reducer as store }      from './store.js';
 import { reducer as navigation } from './navigation.js';
+import { reducer as alert }      from './alert.js';
 
 const rootReducer = combineReducers({
   store,
   navigation,
+  alert,
 });
 
 export default rootReducer;
