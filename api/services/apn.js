a8e8b39b0e17aafd231bf6548626e9121aba5156
Nikhila Ravi <nikkiravi92@gmail.com>
2015-12-22 11:28:47 +0000

    Updates APN server file to set up development and production configs

--- api/services/apn.js
+++ api/services/apn.js
@@ -1,13 +1,18 @@
 /**
  *	Apple Push Notification service, from https://github.com/argon/node-apn.
  *
- *	IMPORTANT NOTES: by default, if the environment variable NODE_ENV=production is set, the module
- *	will connect to the production gateway. Otherwise it will connect to the sandbox.
+ *	IMPORTANT NOTES:
+ *  - by default, if the environment variable NODE_ENV=production is set, the module
+ *	  will connect to the production gateway. Otherwise it will connect to the sandbox.
+ *  - the key supplied to create the apn connection need to be correctly set to either
+ *    development, ad_hoc_production (beta testing) or production (submitting to App store).
+ *  - the certificate is either development or production
+ *
  *
  *	API Events:
  *	- 'error': emitted when an error occurs during initialisation of the module
  *	- 'transmitted': the notification has been trasmitted to Apple, doesn't mean was succesful and can be called several times.
- *	- 'completed': when all the pending notifications had been sent to Apple
+ *	- 'completed': when all the pending notifications have been sent to Apple
  *	- 'transmissionError': when the notification arrives to Apple and encounters some problem there
  *
 **/
@@ -21,11 +26,30 @@ module.exports = {
   sendNotification: sendNotification
 }
 
+/**
+* DEVELOPMENT connection config
+* - uses the development certifcates and key to connect to the development gateway
+**/
+
 var apnConnection = new apn.Connection({
 	production: false,
-	cert: path.join(__dirname, 'cert.pem'),
-	key: path.join(__dirname, 'key.pem'),
+	cert: path.join(__dirname, 'development_cert.pem'),
+	key: path.join(__dirname, 'development_key.pem'),
 });
+
+
+/**
+* AD HOC PRODUCTION connection config
+* - uses the ad hoc production certificate and key to connect to the APN production gateway
+* Comment in the section below for ad hoc production (i.e. beta testing on a limited number of devices)
+**/
+
+// var apnConnection = new apn.Connection({
+// 	production: true,
+// 	cert: path.join(__dirname, 'production_cert.pem'),
+// 	key: path.join(__dirname, 'ad_hoc_production_key.pem'),
+// });
+
 
 /**
  *	Create a apn instance.
