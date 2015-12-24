fec367a66af173ef70d695b35dbc5a035677111c
besartshyti <besartshyti@gmail.com>
2015-12-24 14:57:13 +0000

    create action testModal which will open, wait and close the modal

--- app/actions/modal.js
+++ app/actions/modal.js
@@ -2,10 +2,25 @@
 
 import * as types from '../action_types.js';
 
-export const showModal = (modal) => {
-  return { type: types.SHOW_MODAL, modal: modal };
+export const showModal = text => {
+  return {
+    type: types.SHOW_MODAL,
+    text: text
+  };
 }
 
-export const clearModal = () => {
-  return { type: types.CLEAR_MODAL };
+export const resetModal = () => {
+  return { type: types.RESET_MODAL };
+}
+
+/**
+ * Opens modal and closes it after 2 seconds
+ */
+export const testModal = () => {
+  return (dispatch, getState) => {
+    dispatch(showModal('Wait...'));
+    setTimeout(() => {
+      dispatch(resetModal());
+    }, 1000)
+  }
 }
