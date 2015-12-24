edd9bba7ab4cf5fcba6f7b03f7dfd7b8254b5322
Izaak Sofer <izaakrogan@googlemail.com>
2015-12-24 12:48:19 +0000

    modal reducer and add modal reducer to reducer index

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,11 +4,13 @@ import { combineReducers }       from 'redux';
 import { reducer as store }      from './store.js';
 import { reducer as navigation } from './navigation.js';
 import { reducer as alert }      from './alert.js';
+import { reducer as modal }      from './modal.js';
 
 const rootReducer = combineReducers({
   store,
   navigation,
   alert,
+  modal,
 });
 
 export default rootReducer;
---
+++ app/reducers/modal.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import { SHOW_MODAL, CLEAR_MODAL } from '../action_types.js';
+
+export const intitialState = null;
+
+export const reducer = (state = intitialState, action) => {
+
+  switch (action.type) {
+    case SHOW_MODAL:
+      return action.modal;
+    case CLEAR_MODAL:
+      return null;
+    default:
+      return state
+  }
+}
