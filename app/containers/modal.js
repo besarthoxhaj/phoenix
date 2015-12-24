9377ba0259a286e8d4a665a37b95e1f18547da39
besartshyti <besartshyti@gmail.com>
2015-12-24 15:00:23 +0000

    remove branching in choosing a modal - simplify modal

--- app/containers/modal.js
+++ app/containers/modal.js
@@ -1,27 +1,13 @@
-/*
-Initial ideas for modals. Use case:
-- One or more standard modals which only need custom text and actions
-- One or more custom modals
-*/
-
 'use strict';
 
 import { connect } from 'react-redux/native';
 import React, { Component, PropTypes, Modal, View } from 'react-native';
 // components
-import DefaultModal from '../components/modal/default_modal.js';
-// custom modal container
-import CustomModalContainer from './custom_modals/custom_modal.js';
+import DefaultModal from '../components/modal.js';
 // actions
 import * as ModalActions from '../actions/modal.js';
 
 const actionCreators = ModalActions;
-
-let open=false;
-let text;
-let close;
-let onPress;
-let content;
 
 class ModalsContainer extends Component {
 
@@ -29,39 +15,26 @@ class ModalsContainer extends Component {
     super(props);
   }
 
-  componentWillReceiveProps(props) {
-    switch (props.modal) {
-      case 'default_modal_1':
-        open = true;
-        text = 'Default Modal 1',
-        close = this.props.clearModal,
-        content = <DefaultModal text={text} close={close}/>
-      break;
-      case 'custom_modal':
-        open = true;
-        content = <CustomModalContainer/>
-      break;
-      default:
-        open=false;
-    }
-  }
-
   render() {
     return (
-      <Modal visible={ open }>
-        { content }
+      <Modal visible={this.props.visible} transparent={true}>
+        <DefaultModal text={this.props.text}/>
       </Modal>
     );
   }
 }
 
 ModalsContainer.propTypes = {
-  showModal: PropTypes.func.isRequired,
-  clearModal: PropTypes.func.isRequired,
+  visible: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => {
-  return { modal: state.modal };
+
+  return {
+    visible: state.modal.visible,
+    text: state.modal.text
+  };
 }
 
 export default connect(mapStateToProps, actionCreators)(ModalsContainer);
