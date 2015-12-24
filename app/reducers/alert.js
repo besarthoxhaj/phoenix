de9eb4985d6a9d25fb77bec7805e3c8217081ac8
Izaak Sofer <izaakrogan@googlemail.com>
2015-12-24 12:47:51 +0000

    alert reducer

--- app/reducers/alert.js
+++ app/reducers/alert.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import { ALERT, CLEAR_ALERT } from '../action_types.js';
+import { SHOW_ALERT, CLEAR_ALERT } from '../action_types.js';
 
 export const intitialState = null;
 
 export const reducer = (state = intitialState, action) => {
 
   switch (action.type) {
-    case ALERT:
+    case SHOW_ALERT:
       return action.alert;
     case CLEAR_ALERT:
       return null
