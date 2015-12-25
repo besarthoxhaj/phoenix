e7b2a5c9d04a5581dc3260f2f3250f71d2c358a6
besartshyti <besartshyti@gmail.com>
2015-12-25 09:04:08 +0000

    add router smaller case

---
+++ app/containers/router.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import { connect } from 'react-redux/native';
+import { bindActionCreators } from 'redux';
+import React, { Component, PropTypes, Navigator, View } from 'react-native';
+// container
+import Login from './login.js';
+import Home from './home.js';
+import Header from './header.js';
+import Footer from './footer.js';
+// actions
+import * as StartActions from '../actions/start.js';
+import * as NavigationActions from '../actions/navigation.js';
+
+const actionCreators = {
+  ...StartActions,
+  ...NavigationActions,
+};
+
+class Router extends Component {
+  
+  constructor(props){
+    super(props);
+    this.renderScene = this.renderScene.bind(this);
+  }
+
+  componentWillUpdate(nextProps) {
+    const { route : newRoute, stack: newStack} = nextProps;
+    const { route } = this.props;
+
+    if (newRoute.name !== route.name){
+      this.refs.navigator.jumpTo(newStack[newRoute.index]);
+    }
+  }
+
+  renderScene(route,navigator){
+    switch(route.name){
+      case 'Login':
+        return (<Login />);
+      case 'Home':
+        return (<Home />);
+      default:
+        return (<Login />)
+    }
+  }
+
+  render(){
+    return (
+      <View style={{flex:1}}>
+        <Header />
+        <Navigator
+        //Use Navigator to transition between different scenes in your app.
+          ref='navigator'
+          initialRoute={this.props.stack[this.props.route.index]}
+          initialRouteStack={this.props.stack}
+          renderScene={this.renderScene}
+          history={this.props.history}
+          sceneStyle={{
+            flexDirection: 'column',
+            flex: 2
+          }}
+          configureScene={route => {
+            if (route.sceneConfig) {
+              return route.sceneConfig;
+            } else {
+              return Navigator.SceneConfigs.FadeAndroid;
+            }
+          }}
+        />
+        <Footer />
+      </View>
+    )
+  }
+}
+
+Router.propTypes = {
+  route: PropTypes.object.isRequired,
+  history: PropTypes.array.isRequired,
+  stack: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    route: state.navigation.route,
+    history: state.navigation.history,
+    stack: state.navigation.stack,
+  }
+}
+
+export default connect(mapStateToProps, actionCreators)(Router);
