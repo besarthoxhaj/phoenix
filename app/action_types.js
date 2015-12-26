a85e9e6e35ee361a489b33c114b85c0dd57a20fe
besartshyti <besartshyti@gmail.com>
2015-12-26 23:39:58 +0000

    add action types

--- app/action_types.js
+++ app/action_types.js
@@ -1,6 +1,7 @@
 'use strict';
 
 export const START = 'START';
+export const COMPLETE = 'persist/COMPLETE';
 // navigation
 export const CHANGE_ROUTE = 'CHANGE_ROUTE';
 export const GO_BACK = 'GO_BACK';
@@ -12,4 +13,7 @@ export const SHOW_ALERT = 'SHOW_ALERT';
 export const RESET_ALERT = 'RESET_ALERT';
 // modal
 export const SHOW_MODAL = 'SHOW_MODAL';
-export const RESET_MODAL = 'RESET_MODAL'
+export const RESET_MODAL = 'RESET_MODAL';
+// login
+export const LOGIN_UPDATE_INPUT = 'LOGIN_UPDATE_INPUT';
+export const LOGIN_RESET_INPUT = 'LOGIN_RESET_INPUT';
