df492523cc8828ac9cdb4517b834c6f51a2d395e
besartshyti <besartshyti@gmail.com>
2015-12-26 09:18:43 +0000

    export default

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,7 @@
 
 import { combineReducers } from 'redux';
 import { reducer as store } from './store.js';
-import { reducer as router } from './router.js';
+import router from './router.js';
 import { reducer as alert } from './alert.js';
 import { reducer as modal } from './modal.js';
 
