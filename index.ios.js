0f2e75086dde08726912d83fb07e0a4f21317e97
besartshyti <besartshyti@gmail.com>
2016-01-05 19:11:28 +0000

    new app

--- index.ios.js
+++ index.ios.js
@@ -7,30 +7,70 @@ import React, {
   StyleSheet,
   Text,
   View,
+  PushNotificationIOS,
+  AppStateIOS,
+  AlertIOS,
 } from 'react-native';
 
-import { Provider } from 'react-redux/native';
-import configureStore from './app/configure_store.js';
-import App from './app/containers/app.js';
-const store = configureStore();
-
 class phoenix extends Component {
+
   constructor(props) {
     super(props);
+    this.state = {text: '_'};
   }
+
+  componentWillMount(){
+
+    const that = this;
+
+    PushNotificationIOS.addEventListener('register', function () {
+      console.log('register', arguments, AppStateIOS);
+    });
+
+    PushNotificationIOS.addEventListener('notification', function () {
+      console.log('AppStateIOS',AppStateIOS);
+      if (AppStateIOS.currentState === 'background') {
+        that.setState({text:'background'});
+      } else {
+        that.setState({text:'active'});
+      }
+    });
+
+    PushNotificationIOS.requestPermissions(function () {
+      console.log('requestPermissions', arguments, AppStateIOS);
+    });
+  }
+
+  componentWillUnmount(){
+
+    AppStateIOS.removeEventListener('change', function () {
+      console.log('change', arguments);
+    });
+
+    PushNotificationIOS.removeEventListener('register', function () {
+      console.log('register', arguments, AppStateIOS);
+    });
+
+    PushNotificationIOS.removeEventListener('notification', function () {
+      console.log('notification', arguments, AppStateIOS);
+    });
+  }
+
   render(){
     return (
-      <Provider store={store}>
-        {
-          () => {
-            return (
-              <App />
-            );
-          }
-        }
-      </Provider>
-    )
+      <View style={styles.container}>
+        <Text>{this.state.text}</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 AppRegistry.registerComponent('phoenix', () => phoenix);
