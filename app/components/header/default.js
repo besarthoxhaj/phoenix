af36d0a749e391820522ccc7bbb2ce4fa0d42883
Nikhila Ravi <nikkiravi92@gmail.com>
2016-01-05 18:11:49 +0000

    Adds text to show connection status in the header

--- app/components/header/default.js
+++ app/components/header/default.js
@@ -14,13 +14,14 @@ import H1 from '../helpers/_h1.js';
 
 export default class Header extends Component {
   render(){
-    console.log('test',this.props.router.route.name)
+    console.log('test',this.props)
     return (
       <View style={styles.container}>
         <TouchableHighlight onPress={this.props.goBack}>
           <Text style={{color:'#cccc00'}}>Go Back</Text>
         </TouchableHighlight>
         <H1>{this.props.router.route.name}</H1>
+        <Text>{"CONNECTION STATUS: " + (this.props.ui.isConnected ? 'online' : 'offline') }</Text>
       </View>
     )
   }
@@ -34,7 +35,7 @@ Header.propTypes = {
 const styles = StyleSheet.create({
   container: {
     height: 80,
-    backgroundColor: '#333',
+    backgroundColor: 'pink',
     justifyContent: 'center',
     alignItems: 'center',
     width: Dimensions.get('window').width,
