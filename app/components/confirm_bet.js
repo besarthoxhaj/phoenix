cd1da0c68991c6eaadea04aee04956bf0e908b4e
Izaak Sofer <izaakrogan@googlemail.com>
2016-01-11 12:23:00 +0000

    confirm bet component with redirect and bounce action

---
+++ app/components/confirm_bet.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React, {
+  Text,
+  View,
+  StyleSheet,
+  PropTypes,
+  Component
+} from 'react-native';
+//components
+import Button from './helpers/_button.js';
+
+export default class ConfirmBet extends Component {
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={() => this.props.navigateTo({name: 'deck'}) }
+          text={'Go Back'}
+        />
+        <Button
+          onPress={() => {
+            this.props.noBounceBack(),
+            this.props.sliceTopCard(),
+            this.props.navigateTo({name: 'deck'})
+          }}
+          text={'Confirm'}
+        />
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingHorizontal: 50,
+  }
+});
