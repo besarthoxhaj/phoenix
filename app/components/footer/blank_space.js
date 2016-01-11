06ee351c2ed2884a7d9d65a8fae0a2e6f8f9f86d
Izaak Sofer <izaakrogan@googlemail.com>
2016-01-11 12:25:25 +0000

    add blank space footer and header for handling ugly page transitions between pages with and without header+footer

---
+++ app/components/footer/blank_space.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React, {
+  Component,
+  View,
+} from 'react-native';
+
+export default class Footer extends Component {
+  render(){
+    return (
+      <View style={{height:80, backgroundColor:'transparent'}}/>
+    )
+  }
+}
---
+++ app/components/header/blank_space.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React, {
+  Component,
+  View,
+} from 'react-native';
+
+export default class Header extends Component {
+  render(){
+    return (
+      <View style={{height:80, backgroundColor:'transparent'}}/>
+    )
+  }
+}
