73b037ac4c3daf644b2def242055188e092c1997
Izaak Sofer <izaakrogan@googlemail.com>
2016-01-11 12:22:34 +0000

    simplify deck action

--- app/actions/deck.js
+++ app/actions/deck.js
@@ -2,22 +2,18 @@ import fetch from 'isomorphic-fetch';
 
 import * as types from '../action_types.js';
 
-function sliceTopCard () {
+export function returnDeck () {
+  return { type: types.RETURN_DECK }
+}
+
+export function sliceTopCard () {
   return { type: types.NEXT_CARD };
-};
+}
 
-export function nextCard () {
-  return (dispatch, getState) => {
-    return dispatch(sliceTopCard());
-  }
-};
+export function bounceBack () {
+  return { type: types.BOUNCE_BACK };
+}
 
-export function swipeLeft () {
-  console.log('swiped left')
-};
-
-export function swipeRight () {
-  return (dispatch, getState) => {
-    return dispatch(nextCard());
-  }
-};
+export function noBounceBack () {
+  return { type: types.NO_BOUNCE_BACK };
+}
