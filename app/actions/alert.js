7755540ec8dc03f43cd9b078d324c7a77e73c69c
besartshyti <besartshyti@gmail.com>
2016-01-17 15:52:51 +0000

    intercept default button

--- app/actions/alert.js
+++ app/actions/alert.js
@@ -2,17 +2,30 @@
 
 import * as types from '../action_types.js';
 
-export const show = (title, message, buttons = []) => {
-  return {
-    type: types.SHOW_ALERT,
-    title,
-    message,
-    buttons,
-  };
+export const showAlert = (title, message, buttons = []) => {
+  return {type: types.SHOW_ALERT, title, message, buttons};
 }
 
 export const reset = () => {
   return { type: types.RESET_ALERT, isVisible: false };
+}
+
+export const show = (title, message, buttons) => dispatch => {
+
+  // make sure the alert has a
+  // redux button
+  if (!buttons) {
+    buttons = [{
+      text: 'Ok',
+      onPress: () => {
+        dispatch(reset());
+      }
+    }];
+  }
+
+  setTimeout(() => {
+    dispatch(showAlert(title, message, buttons));
+  },0);
 }
 
 export const testAlert = () => {
