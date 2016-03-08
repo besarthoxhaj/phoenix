f23efc4fcf476a7f88c9d57dded1ab4deb70325f
besarthoxhaj <besartshyti@gmail.com>
2016-03-08 11:34:14 +0000

    style

--- app/containers/login.js
+++ app/containers/login.js
@@ -3,9 +3,7 @@
 import { connect } from 'react-redux/native';
 import { bindActionCreators } from 'redux';
 import React, { Component, PropTypes } from 'react-native';
-// components
 import Login from '../components/login.js';
-// actions
 import * as NavigationActions from '../actions/router.js';
 import * as LoginActions from '../actions/login.js';
 
@@ -22,14 +20,6 @@ class LoginContainer extends Component {
   }
 }
 
-LoginContainer.propTypes = {
-  navigateTo: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => {
-  return {
-    ...state,
-  };
-};
+const mapStateToProps = state => ({...state});
 
 export default connect(mapStateToProps, actionCreators)(LoginContainer);
